aa5a22898ff336ecbe22f500f230781ceec32e74
Kevin Le Scouarnec <kevinlescouarnec@Ordinateur-portable-de-Kevin.local>
2023-11-30 14:51:44 -0500

    feat(organization): add organization entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrganizationModule } from './organization/organization.module';
+import { OrganizationController } from './organization/organization.controller';
+import { OrganizationModule } from './organization/organization.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [OrganizationModule],
+  controllers: [AppController, OrganizationController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/organization/dto/create-organization.dto.ts
@@ -0,0 +1,9 @@
+import { IsInt, IsString } from 'class-validator';
+
+export class CreateOrganizationDto {
+  @IsInt()
+  id: number;
+
+  @IsString()
+  name: string;
+}
